6697b121c83b2d2ce3a509c25ea7baef6a31089d
fake-news-bot1 <70606273+fake-news-bot1@users.noreply.github.com>
2020-09-03 12:26:53 -0700

    Add files via upload

--- functions/reddit_conn.py
+++ functions/reddit_conn.py
@@ -13,57 +13,80 @@ def connect():
                          password=credentials.password)
 
     already_replied = load_pickle()
-    # already_replied = []
-    print(f'Replied to: {already_replied}')
 
-    subreddit = reddit.subreddit('testingground4bots')
-    # subreddit = reddit.subreddit('worldnews')
+    subreddit = reddit.subreddit('worldnews+news+politics+testingground4bots')
 
     # phrase to activate the bot
     keyphrase = '!FakeNewsBot'
 
     while True:
-        for comment in subreddit.stream.comments():
+        for comment in subreddit.stream.comments(skip_existing=True):
             try:
-                if comment.submission.url:
+                # Skip comment if no article URL found
+                if comment.submission.url and comment.body:
                     url = comment.submission.url
                 else:
                     continue
-                print(f'Commenter ID: {comment}, comment: {comment.body}')
-                if keyphrase in comment.body and comment not in already_replied:
+    
+                comment_id = [comment.submission, comment]
+    
+                # If bot is called and has not already replied to comment, scan article and reply
+                if keyphrase in comment.body and comment_id not in already_replied:
                     article_text = scraper.get_data(url)
-                    msg = analyzer.analyze(article_text)
+                    result = analyzer.analyze(article_text)
 
-                    print(f'post: {url} \ntext: {article_text} \nresult: {msg}')
-                    # comment.reply(msg)
-                    # already_replied.append(comment)
-                    print(f'Replied to: {already_replied}')
-                    time.sleep(60)
+                    form_reply(comment, result)
+
+                    already_replied.append(comment_id)
+                    save_pickle(already_replied)
+                    print(f'URL: {url} \n\nResult: {result}')
+            except (TypeError, praw.exceptions.RedditAPIException):
+                pass
             except TypeError:
-                print(comment.submission.url)
-                print("nope")
-                continue
-            # finally:
-            #     save_pickle(already_replied)
-                
+                msg = 'Oops, looks like something has gone wrong. Please try again in a few minutes.'
+                form_reply(comment, msg)
 
+
+# Load 'replied.pickle' object and comments list, otherwise create new pickle
 def load_pickle():
     try:
-        return pickle.load(open('replied.pickle', 'rb'))
-    except Exception as e:
-        print(e)
+        file_handler = open('replied.pickle', 'rb')
+        comments = pickle.load(file_handler)
+        file_handler.close()
+
+        return comments
+
+    except FileNotFoundError:
         create_pickle()
 
 
+# Create 'replied.pickle' object and return empty list
 def create_pickle():
-    arr = []
-    pickle.dump(arr, open('replied.pickle', 'wb')) 
-    return arr
+    comments = []
+
+    file_handler = open('replied.pickle', 'wb')
+    pickle.dump(comments, file_handler)
+    file_handler.close()
+
+    return comments
 
 
-def save_pickle(arr):
-    pickle.dump(arr, open('replied.pickle', 'wb'))
-    
+# Save list of  replied comments to 'replied.pickle' object
+def save_pickle(comments):
+    file_handler = open('replied.pickle', 'wb')
+    pickle.dump(comments, file_handler)
+    file_handler.close()
+
+
+def form_reply(comment, result):
+    msg = f'{result} \n_____ \n\nThis bot scrapes news articles and analyzes language patterns \
+    using machine learning packages for bias and misinformation with roughly 80% accuracy on unseen articles. \
+    \n\n\nThe Passive Aggressive Classifier runs on a training dataset of 20,799 articles found\
+      [here](https://www.kaggle.com/c/fake-news/data?select=train.csv), and the  source code can be found\
+      [here](https://github.com/fake-news-bot1/fake-news-bot).'
+
+    comment.reply(msg)
+
 
 if __name__ == '__main__':
     connect()
